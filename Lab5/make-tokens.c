6980d96af47cd4b3fbf062c3fab5f37a2e91c0fb
Krishna Harsha <krishna.harsha.reddy@gmail.com>
2016-02-12 14:29:10 +0530

    Added server

--- Lab5/make-tokens.c
+++ Lab5/make-tokens.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
+#include <stdbool.h>
 #define MAX_INPUT_SIZE 1024
 #define MAX_TOKEN_SIZE 64
 #define MAX_NUM_TOKENS 64
@@ -55,19 +55,44 @@ int cd(char **args)
   return 1;
 }
 
+
+int server(char **args, char ** serverinfo)
+{
+	if(args[1]==NULL || args[2]==NULL)
+	{
+		fprintf(stderr, "Too less arguments. Argument should be of form \"server <server-IP> <server-port>\"\n");
+		return -1;
+	}
+	else if(args[3]!=NULL)
+	{
+		fprintf(stderr, "Too many arguments. Argument should be of form \"server <server-IP> <server-port>\"\n");
+		return -2;
+	}
+	else
+	{
+		serverinfo[0]=args[1];
+		serverinfo[1]=args[2];
+	}
+	return 1;
+}
+
 void  main(void)
 {
 	char  line[MAX_INPUT_SIZE];            
 	char  **tokens;              
 	int i;
-
+	
+	char * serverinfo[2];
+	bool serverInitialized=0;
+	
+	
 	while (1)
  {           
 	 
 		printf("Hello>");     
 		bzero(line, MAX_INPUT_SIZE);
 		gets(line);           
-		printf("Got command %s\n", line);
+		//~ printf("Got command %s\n", line);
 		line[strlen(line)] = '\n'; //terminate with new line
 		tokens = tokenize(line);
 
@@ -75,6 +100,15 @@ void  main(void)
 		if(strcmp(tokens[0],"cd") == 0)
 		{
 			cd(tokens);
+		}
+		else if(strcmp(tokens[0],"server")==0)
+		{
+			int a=server(tokens,serverinfo);
+			if(a>=0)
+			{
+				serverInitialized=1;
+				printf("Server details changed succesfully.\nIP: %s \nPort: %s \n",serverinfo[0],serverinfo[1]);
+			}
 		}
 		else
 		{
