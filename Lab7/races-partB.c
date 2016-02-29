4f78d5d719f368c6fd8227a27f26a621a5ea03b4
K KRISHNA HARSHA REDDY <krishna.harsha@osl-17.cse.iitb.ac.in>
2016-02-29 14:15:14 +0530

    Finished lab in 1 min

---
+++ Lab7/races-partB.c
@@ -0,0 +1,65 @@
+/*
+** Client program to recieve the file
+*/
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <time.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
+#define MAXDATASIZE 512
+#define MAXTHREADS 300
+
+int count;
+int N;
+int k;
+int locked;
+struct argstruct {
+  char** argv;
+  int id;
+};  
+
+void *update(void* args)
+{
+	int i;
+	for(i=0;i<k;i++)
+	{
+	while(locked);
+		locked=1;
+		count++;
+		locked=0;
+	
+	}
+}
+
+int main(int argc, char* argv[])
+{
+  N = atoi(argv[1]);
+  k = atoi(argv[2]);
+  locked=0;
+  pthread_t thr[MAXTHREADS];
+  int i;
+  struct timeval i1, f1;
+  struct argstruct args[MAXTHREADS];
+  gettimeofday(&i1,NULL);
+  for (i = 0; i<N; i++)  {
+    pthread_create(&thr[i], NULL, update, NULL);
+  }
+  for (i=0; i<N; i++) {
+    pthread_join(thr[i],NULL);
+  }
+  gettimeofday(&f1,NULL);
+  printf("Time: %lu\n",(f1.tv_sec*1000000+f1.tv_usec)-(i1.tv_sec*1000000+i1.tv_usec));
+  printf("Count: %d\n",count);
+  return 0;
+}
+
